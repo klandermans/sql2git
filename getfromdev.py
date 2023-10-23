4455180e057eaef627a2738fcc0e37bf3f80c00e
Bert Klandermans <klandermans@users.noreply.github.com>
2023-10-23 15:57:18 +0200

    Update getfromdev.py

--- getfromdev.py
+++ getfromdev.py
@@ -13,6 +13,30 @@ def export(type):
             FROM SYS.SQL_Modules As SM 
             INNER JOIN SYS.Objects As Obj ON SM.Object_ID = Obj.Object_ID 
             WHERE Obj.Type = '"""+type+"""' and left(name,3)<>'sp_' COLLATE Latin1_General_CS_AS """
+
+    if type == 'T':
+        sql = """
+SELECT QUOTENAME(s.name) + '.' + QUOTENAME(t.name) , 
+    'CREATE TABLE ' + QUOTENAME(s.name) + '.' + QUOTENAME(t.name) + CHAR(13) + CHAR(10) + '(' +
+    STUFF((
+        SELECT 
+            CHAR(13) + CHAR(10) + '    ' + c.name + ' ' + 
+            CASE 
+                WHEN c.is_computed = 1 THEN 'AS ' + OBJECT_DEFINITION(c.default_object_id)
+                ELSE t.name
+            END + ' ' +
+            CASE 
+                WHEN c.is_nullable = 1 THEN 'NULL'
+                ELSE 'NOT NULL'
+            END + ',' 
+        FROM sys.columns c
+        WHERE c.object_id = t.object_id
+        FOR XML PATH(''), TYPE
+    ).value('.', 'NVARCHAR(MAX)'), 1, 5, '') +
+    CHAR(13) + CHAR(10) + ');'
+FROM sys.tables t
+INNER JOIN sys.schemas s ON t.schema_id = s.schema_id;
+"""        
     
     # gewoon alles verwijderen, de sql server is leidend. 
     # Eventueel een git add en git commit om de huidige versie in git te zetten. 
@@ -31,6 +55,7 @@ def export(type):
 # FROM
 #     dairycampusplus.INFORMATION_SCHEMA.columns
 
+export('T')
 export('P')
 export('V')
 export('FN')
